27425e54857bf158d735f18cee706343fd179153
wneo <wuyuan2012@gmail.com>
2018-07-05 22:17:33 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <cfloat>
+
+#include "vec3.h"
+#include "ray.h"
+#include "hitable.h"
+#include "hitablelist.h"
+#include "material.h"
+#include "sphere.h"
+#include "camera.h"
+
+using namespace std;
+
+
+
+vec3 color(const ray &r, hitable *world, int depth) {
+	hit_record rec;
+	if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+		ray scattered;
+		vec3 attenuation;
+		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+			return attenuation * color(scattered, world, depth+1);
+		}else{
+			return vec3(0, 0, 0);
+		}
+	}else{
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5 * (unit_direction.y() + 1.0);
+		return (1.0-t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+	}
+}
+
+int main() {
+	int nx = 200;
+	int ny = 100;
+	int ns = 10;
+
+	cout << "P3\n" << nx << " " << ny << "\n255\n";
+
+	hitable *list[5];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.3, 0.3)));
+	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
+	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+	list[4] = new sphere(vec3(-1, 0, -1), -0.25, new dielectric(1.5));
+
+	hitable *world = new hitable_list(list, 5);
+
+	camera cam;
+
+
+	for (int j = ny; j >0; --j) {
+		for (int i = 0; i < nx; ++i) {
+			vec3 col(0, 0, 0);
+			for (int s = 0; s < ns; ++s) {
+				float u = float(i + drand48()) / float(nx);
+				float v = float(j + drand48()) / float(ny);
+				ray r = cam.get_ray(u, v);
+				//vec3 p = r.point_at_parameter(2.0);
+				col += color(r, world, 0);
+			}
+
+
+			col/=float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+
+			cout << int(255.99 * col.r()) << " "  << int(255.99 *col.g()) << " " << int(255.99 * col.b()) << "\n";
+		}
+	}
+}
